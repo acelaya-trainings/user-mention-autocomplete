b970c3703becad051f613d5ab698b9c28a042250
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-10-31 11:08:22 +0100

    Improved TextArea test

--- src/comment-widget/TextArea.spec.tsx
+++ src/comment-widget/TextArea.spec.tsx
@@ -4,6 +4,39 @@ import userEvent from '@testing-library/user-event';
 import { TextArea } from './TextArea';
 import { User } from '../data';
 
+const users: User[] = [
+  {
+    name: 'User',
+    username: 'user1',
+    avatar_url: 'https://secure.gravatar.com/avatar/cd4318b7fb1cf64648f59198aca8757f?d=mm',
+  },
+  {
+    name: 'Foo Foo',
+    username: 'foo',
+    avatar_url: 'https://secure.gravatar.com/avatar/cd4318b7fb1cf64648f59198aca8757f?d=mm',
+  },
+  {
+    name: 'Contains user',
+    username: 'someone',
+    avatar_url: 'https://secure.gravatar.com/avatar/cd4318b7fb1cf64648f59198aca8757f?d=mm',
+  },
+  {
+    name: 'Bar Bar',
+    username: 'bar',
+    avatar_url: 'https://secure.gravatar.com/avatar/cd4318b7fb1cf64648f59198aca8757f?d=mm',
+  },
+  {
+    name: 'User2',
+    username: 'user2',
+    avatar_url: 'https://secure.gravatar.com/avatar/cd4318b7fb1cf64648f59198aca8757f?d=mm',
+  },
+  {
+    name: 'R is the first letter',
+    username: 'user3',
+    avatar_url: 'https://secure.gravatar.com/avatar/cd4318b7fb1cf64648f59198aca8757f?d=mm',
+  },
+];
+
 const setUp = (loadUsers: () => Promise<User[]>) => ({
   user: userEvent.setup(),
   ...render(<TextArea loadUsers={loadUsers} />),
@@ -12,6 +45,22 @@ const setUp = (loadUsers: () => Promise<User[]>) => ({
 test('renders loading status in dropdown until users have been loaded', async () => {
   const { user } = setUp(() => new Promise(resolve => setTimeout(() => resolve([]), 2000)));
 
-  await user.type(screen.getByPlaceholderText('Write a comment...'), 'I totally agree @user');
+  await user.type(screen.getByPlaceholderText('Write a comment...'), 'I totally agree, @user');
   expect(screen.getByText('Loading users...')).toBeInTheDocument();
+});
+
+test.each([
+  ['user', ['User', 'User2', 'R is the first letter', 'Contains user']],
+  ['foo', ['Foo Foo']],
+  ['r', ['R is the first letter', 'User', 'Contains user', 'Bar Bar', 'User2']],
+])('shows filtered list of users based on input after "@" character', async (searchTerm, expectedUsers) => {
+  const { user } = setUp(() => Promise.resolve(users));
+
+  await user.type(screen.getByPlaceholderText('Write a comment...'), `I totally agree, @${searchTerm}`);
+  const images = screen.getAllByRole('img');
+
+  expect(images).toHaveLength(expectedUsers.length);
+  expectedUsers.forEach((user, index) => {
+    expect(images[index]).toHaveAttribute('alt', expect.stringContaining(`"${user}"`));
+  });
 });
--- src/comment-widget/UsersDropdown.tsx
+++ src/comment-widget/UsersDropdown.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, Fragment } from 'react';
 import { User } from '../data';
 import './UsersDropdown.css';
 
@@ -10,7 +10,11 @@ interface UsersDropdownProps {
 const HighlightText: FC<{ text: string, filteringText: string }> = ({ text, filteringText }) => (
   <>
     {text.split(filteringText).map(
-      (part, index, array) => array.length === index + 1 ? part : <>{part} <span className="comment-widget-users-dropdown__list-item--highlight">{filteringText}</span></>
+      (part, index, array) => (
+        <Fragment key={`${part}_${index}`}>
+          {array.length === index + 1 ? part : <>{part} <span className="comment-widget-users-dropdown__list-item--highlight">{filteringText}</span></>}
+        </Fragment>
+      )
     )}
   </>
 )
@@ -20,9 +24,9 @@ export const UsersDropdown: FC<UsersDropdownProps> = ({ users, filteringText }) 
     <>
       {[
         ...users.filter(({ name, username }) => name.toLowerCase().startsWith(filteringText) || username.toLowerCase().startsWith(filteringText)),
-        ...users.filter(({ name, username }) => (!name.toLowerCase().startsWith(filteringText) && name.toLowerCase().includes(filteringText)) || (!username.toLowerCase().startsWith(filteringText)) && username.toLowerCase().includes(filteringText)),
+        ...users.filter(({ name, username }) => !name.toLowerCase().startsWith(filteringText) && !username.toLowerCase().startsWith(filteringText) && (name.toLowerCase().includes(filteringText) || username.toLowerCase().includes(filteringText))),
       ].slice(0, 10).map(((user, index) => (
-        <div key={`${user.username}_${index}`} className="comment-widget-users-dropdown__list-item">
+        <div key={`${user.username}_${user.name}_${user.avatar_url}`} className="comment-widget-users-dropdown__list-item">
           <img src={user.avatar_url} alt={`Avatar for user "${user.name}"`} className="comment-widget-users-dropdown__list-avatar-img" />
           <HighlightText text={user.name} filteringText={filteringText} /> (<HighlightText text={user.username} filteringText={filteringText} />)
         </div>
